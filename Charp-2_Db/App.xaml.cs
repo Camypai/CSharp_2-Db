34a2aa00b74d83540d31d832d20ae62aff6e78d6
Илья <camypai.13@list.ru>
2019-03-14 17:46:11 +0300

    Initialization

---
+++ Charp-2_Db/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Charp_2_Db
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ Charp-2_Db/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Charp_2_Db
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
